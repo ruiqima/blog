52f997733c96cd022a191c9f169c6c5dbe0c2366
malechiMLC <3054487172@qq.com>
2020-04-19 22:16:29 +0800

    rebuilding site 2020年04月19日 22:16:27

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Set commit message
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Build the project.
+hugo --gc --minify # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+git push coding master
